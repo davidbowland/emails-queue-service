81f7693f162b58e1da7c6767eb3c98f6240f3fd6
David Bowland <dbowland1@gmail.com>
2021-12-20 01:35:24 -0600

    Remove original log message

--- src/handlers/sqs-payload-processor.ts
+++ src/handlers/sqs-payload-processor.ts
@@ -1,6 +1,7 @@
 import { SQSEvent, SQSHandler, SQSRecord } from 'aws-lambda'
 
 import { generateEmailFromData, sendRawEmail } from '../services/ses'
+import { handleErrorNoDefault } from '../util/error-handling'
 import { getDataFromRecord } from '../util/message-processing'
 
 /* Queue processing */
@@ -8,9 +9,8 @@ import { getDataFromRecord } from '../util/message-processing'
 export const processSingleMessage = (record: SQSRecord) =>
   getDataFromRecord(record).then((data) => generateEmailFromData(data.email).then((email) => sendRawEmail(email)))
 
-export const sqsPayloadProcessorHandler: SQSHandler = (event: SQSEvent) => {
-  // All log statements are written to CloudWatch by default. For more information, see
-  // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-  console.info(JSON.stringify(event))
-  return Promise.all(event.Records.map((record) => exports.processSingleMessage(record))).then(() => undefined)
-}
+export const sqsPayloadProcessorHandler: SQSHandler = (event: SQSEvent) =>
+  event.Records.reduce(
+    (previous, record) => previous.then(() => exports.processSingleMessage(record).catch(handleErrorNoDefault())),
+    Promise.resolve(undefined)
+  )
