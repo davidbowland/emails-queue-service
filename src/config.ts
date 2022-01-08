84525b163f2107e1bf7f457fcfd05431e5f4aab9
David Bowland <dbowland1@gmail.com>
2022-01-08 13:20:57 -0600

    Modify notification email

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,4 @@ export const emailBucket = process.env.EMAIL_BUCKET as string
 
 export const emailRegion = process.env.EMAIL_REGION as string
 export const notificationFrom = 'do-not-reply@bowland.link'
-export const notificationTarget = 'emails-queue-service-error@bowland.link'
+export const notificationTarget = 'dbowland1+emails-queue-service-error@gmail.com'
