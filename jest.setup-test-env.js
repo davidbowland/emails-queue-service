debc091ba34aa01ae3c86c666d3c5807d8111129
David Bowland <dbowland1@gmail.com>
2022-01-24 19:12:47 -0600

    Only mock console in logging test

--- jest.setup-test-env.js
+++ jest.setup-test-env.js
@@ -5,10 +5,3 @@ process.env.EMAIL_BUCKET = 'test-bucket'
 // SES
 
 process.env.EMAIL_REGION = 'us-east-1'
-
-// Console
-
-console.info = jest.fn()
-console.log = jest.fn()
-console.warn = jest.fn()
-console.error = jest.fn()
