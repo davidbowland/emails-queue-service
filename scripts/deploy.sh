ddeb9ac3cf8bbe1e459203b5414ad0bb304717fd
David Bowland <dbowland1@gmail.com>
2022-02-08 01:12:47 -0600

    Correct deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,8 +14,8 @@ sam build --template ${SAM_TEMPLATE} --use-container
 
 # Deploy build lambda
 
-TESTING_ARTIFACTS_BUCKET=emails-lambda-source
-TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/aws-sam-cli-managed-dev-p-CloudFormationExecutionR-1VAGXTZ6YPX38"
+TESTING_ARTIFACTS_BUCKET=aws-sam-cli-managed-emails-queue-artifactsbucket-occwq1o9816t
+TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/aws-sam-cli-managed-email-CloudFormationExecutionR-1XI44HO3NYW46"
 TESTING_STACK_NAME=emails-queue-service-test
 sam deploy --stack-name ${TESTING_STACK_NAME} \
            --capabilities CAPABILITY_IAM \
