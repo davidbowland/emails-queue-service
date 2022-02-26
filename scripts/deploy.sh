17745c3067183019ce7e63fdb58951320eb3b4c8
David Bowland <dbowland1@gmail.com>
2022-02-25 23:22:04 -0600

    Use dedicated pipeline user

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,13 +14,14 @@ sam build --template ${SAM_TEMPLATE} --use-container
 
 # Deploy build lambda
 
-TESTING_ARTIFACTS_BUCKET=aws-sam-cli-managed-emails-queue-artifactsbucket-occwq1o9816t
-TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/aws-sam-cli-managed-email-CloudFormationExecutionR-1XI44HO3NYW46"
+TESTING_ARTIFACTS_BUCKET=emails-lambda-test
+TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/emails-cloudformation-test"
 TESTING_STACK_NAME=emails-queue-service-test
 sam deploy --stack-name ${TESTING_STACK_NAME} \
            --capabilities CAPABILITY_IAM \
            --region us-east-1 \
            --s3-bucket ${TESTING_ARTIFACTS_BUCKET} \
+           --s3-prefix emails-queue-service-test \
            --no-fail-on-empty-changeset \
            --role-arn ${TESTING_CLOUDFORMATION_EXECUTION_ROLE} \
            --parameter-overrides "Environment=test"
