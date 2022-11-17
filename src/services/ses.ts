93feee49d53ca0343c530d2c1dac809a3365ba93
David Bowland <dbowland1@gmail.com>
2022-11-16 22:24:59 -0600

    Enable BCC capabilities

--- src/services/ses.ts
+++ src/services/ses.ts
@@ -10,11 +10,13 @@ const ses = xrayCapture(new SES({ apiVersion: '2010-12-01', region: emailRegion 
 /* General */
 
 export const generateEmailFromData = (data: EmailData): Promise<Buffer> =>
-  new Promise((resolve, reject) =>
-    new MailComposer(data)
-      .compile()
-      .build((err: Error | null, message: Buffer) => (err ? reject(err) : resolve(message)))
-  )
+  new Promise((resolve, reject) => {
+    const composer = new MailComposer(data).compile()
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore See https://nodemailer.com/extras/mailcomposer/#bcc
+    composer.keepBcc = true
+    composer.build((err: Error | null, message: Buffer) => (err ? reject(err) : resolve(message)))
+  })
 
 export const sendRawEmail = (message: Buffer): Promise<SES.SendRawEmailResponse> =>
   ses.sendRawEmail({ RawMessage: { Data: message } }).promise()
